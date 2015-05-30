7888602778372b865e87f69b45d7a77611d566a2
Iván Corrales Solera <ivan.corrales.solera@gmail.com>
2015-05-30 09:16:35 +0200

    feat: master branch will only contain the README file

--- src/main/scala/com/wesovi/demo/DemoServiceSystem.scala
+++
@@ -1,5 +0,0 @@
-package com.wesovi.demo
-
-import com.wesovi.demo.api.Api
-
-object DemoServiceSystem extends App with BootedCore with CoreActors with Api
--- src/main/scala/com/wesovi/demo/util/ConfigHolder.scala
+++
@@ -1,7 +0,0 @@
-package com.wesovi.demo.util
-
-import com.typesafe.config.ConfigFactory
-
-trait ConfigHolder { 
-  val config = ConfigFactory.load()
-}
